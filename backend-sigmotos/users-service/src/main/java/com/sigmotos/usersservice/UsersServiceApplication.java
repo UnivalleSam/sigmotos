e5adb4310bb16610b175fa9d35e9433ba1dbbcab
Juanito215 <juan.david.cuellar@correounivalle.edu.co>
2026-05-14 21:50:00 -0500

    se configura la base de datos de users

--- backend-sigmotos/users-service/src/main/java/com/sigmotos/usersservice/UsersServiceApplication.java
+++ backend-sigmotos/users-service/src/main/java/com/sigmotos/usersservice/UsersServiceApplication.java
@@ -2,12 +2,13 @@ package com.sigmotos.usersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UsersServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsersServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
 }
